2e74c984579165cf77348e94afa1ce1425ecdcdb
matherdy <dmather3734@sdsu.edu>
2021-09-24 19:52:52 -0700

    making changes to group by and fixing calculation on batting averages

--- Assignments/Assignment2.sql
+++ Assignments/Assignment2.sql
@@ -1,5 +1,5 @@
 USE baseball;
-#sets baseball as the current database
+#sets baseball as the database
 
 
 #This calculates the batting average for every player by averaging each of their batting averages
@@ -7,7 +7,7 @@ USE baseball;
 
 DROP TABLE IF EXISTS `batting_avg`; #drops table so I can run the query repeatedly without manually deleting table
 CREATE TABLE batting_avg
-  AS (SELECT batter, ROUND(AVG(Hit/atBat),3) ba #ba is batting average
+  AS (SELECT batter, ROUND(SUM(Hit)/SUM(atBat),3) ba #ba is batting average
     FROM batter_counts
     WHERE atBat > 0 #makes sure that the player has at least 1 atbat to fix divide by 0 error
     GROUP BY batter #groups each batter ID together to get an average for each player
@@ -17,7 +17,7 @@ CREATE TABLE batting_avg
 #can group by the year as well as the batter. This makes it an annual batting average.
 DROP TABLE IF EXISTS `batting_avg_annual`;
 CREATE TABLE batting_avg_annual
-  AS (SELECT batter, ROUND(AVG(Hit/atBat),3) ba, YEAR(local_date) year
+  AS (SELECT batter, ROUND(SUM(Hit)/SUM(atBat),3) ba, YEAR(local_date) year
     FROM batter_counts b JOIN game g on (b.game_id = g.game_id) # makes the join on matching the game_ids
     WHERE atBat > 0
     GROUP BY batter, year
@@ -34,11 +34,11 @@ CREATE TABLE rolling_1
     WHERE atBat > 0;
 
 #This query takes the table made in the previous query and joins it on itself so I can get two dates to compare to each
-#other so I can calculate the rolling average. 
+#other so I can calculate the rolling average.
 DROP TABLE IF EXISTS `batting_avg_100_day`;
 CREATE TABLE batting_avg_100_day AS
-    SELECT r1.batter, r1.game_id, r1.local_date, AVG(r2.Hit/r2.atBat)
-    FROM rolling_1 r1, rolling_1 r2
-    WHERE r2.local_date between date_add(r1.local_date, INTERVAL -100 DAY) and r1.local_date
-    GROUP BY r1.batter;
+    SELECT a.batter, a.game_id, a.local_date, ROUND(SUM(b.Hit)/SUM(b.atBat),3)
+    FROM rolling_1 a JOIN rolling_1 b ON
+    b.local_date BETWEEN date_add(a.local_date, INTERVAL -100 DAY) AND a.local_date
+    GROUP BY a.batter AND a.game_id AND a.local_date;
 
